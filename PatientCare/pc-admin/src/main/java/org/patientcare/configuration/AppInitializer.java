2872e00a9f9ae98a359d1f71866f43390d5fe641
Vinay Mishra <mishravinay112@gmail.com>
2016-10-30 14:36:47 +0530

    Working hello world demo with spring 4 and javaconfig without xml and index.jsp

--- PatientCare/pc-admin/src/main/java/org/patientcare/configuration/AppInitializer.java
+++ PatientCare/pc-admin/src/main/java/org/patientcare/configuration/AppInitializer.java
@@ -3,13 +3,12 @@ package org.patientcare.configuration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration.Dynamic;//we need servlet-api above 2.5
+
 //all org.springframework.web is available in spring-web maven dependancy except those which is highlighted
 import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;//this is in spring-webmvc package
-
-import org.patientcare.configuration.AppConfig;
 
 public class AppInitializer implements WebApplicationInitializer
 {
@@ -17,16 +16,15 @@ public class AppInitializer implements WebApplicationInitializer
 							
 	public void onStartup(ServletContext servletContext) throws ServletException 
 	{
-		//spring-web
+		//spring-web   
 		AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
 		ctx.register(AppConfig.class);
 		servletContext.addListener(new ContextLoaderListener(ctx));
-
 		ctx.setServletContext(servletContext);
 
 		Dynamic servlet = servletContext.addServlet(DISPATCHER_SERVLET_NAME, new DispatcherServlet(ctx));
-		servlet.addMapping("/");
 		servlet.setLoadOnStartup(1);
+		servlet.addMapping("/");
 	}
 
 }
---
+++ PatientCare/pc-admin/src/main/java/org/patientcare/configuration/ApplicationInitializer.java
@@ -0,0 +1,26 @@
+package org.patientcare.configuration;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;//spring-webmvc
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+ 
+    public void onStartup(ServletContext container) throws ServletException {
+ 
+    	System.out.println("yaha tak aaya");
+        AnnotationConfigWebApplicationContext ctx = new AnnotationConfigWebApplicationContext();
+        ctx.register(ApplicationConfigurerAdapter.class);
+        ctx.setServletContext(container);
+ 
+        ServletRegistration.Dynamic servlet = container.addServlet("dispatcher", new DispatcherServlet(ctx));
+ 
+        servlet.setLoadOnStartup(1);
+        servlet.addMapping("/");
+    }
+ 
+}
